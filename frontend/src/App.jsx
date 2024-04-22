ddccd4391a9f678d66689b2d166235648b30933c
balbesina228 <dolphie.guy@gmail.com>
2024-04-22 17:43:04 +0700

    fix front back interaction App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import CountryCard from "./components/CountryCard.jsx"
 import {Menu, Spin} from 'antd';
 import axios from 'axios';
 
@@ -20,28 +21,25 @@ const App = () => {
   const onClick = (e) => {
     console.log('click ', e);
   };
-
-  const fetchCountries = () => {
-    axios.get("http://localhost:8000/countries").then(r => {
-      console.log(`r=${r}`)
+  useEffect(() => {
+  const fetchCountries = async () => {
+    await axios.get("http://127.0.0.1:8000/countries").then(r => {
       const countriesResponse = r.data
-      const menuItems = [
-        getItem('Countries list', 'g1', null,
-          countriesResponse.map(c => {
-            {label: c.name}
-          }),
-          'group'
-        )
-      ]
+      console.log(`countriesResponse=${countriesResponse}`)
+
+      const menuItems = countriesResponse.map(c => {
+        return getItem(c.name.common, c.name.common, null, null);
+      });
+
       setCountries(menuItems)
     })
   }
 
-  useEffect(() => {
     fetchCountries()
   }, []);
 
   return (
+  <>
   <div className="flex">
     <Menu
       onClick={onClick}
@@ -55,6 +53,8 @@ const App = () => {
       className="h-screen overflow-scroll"
     />
     </div>
+    <CountryCard/>
+    </>
   );
 };
 export default App;
